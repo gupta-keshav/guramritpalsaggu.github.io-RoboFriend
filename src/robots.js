f4ed7addd13c30445bf1bf592811796cfc140207
Paaji <paaji@pop-os.localdomain>
2018-09-01 17:29:33 +0545

    first commit

---
+++ src/robots.js
@@ -0,0 +1,47 @@
+export const robots=[
+  {
+    "id": "1",
+    "name": "Flowers Alvarado",
+    "email": "flowersalvarado@assitia.com"
+  },
+  {
+    "id": "2",
+    "name": "Thompson Banks",
+    "email": "thompsonbanks@assitia.com"
+  },
+  {
+    "id": "3",
+    "name": "Beatrice Barry",
+    "email": "beatricebarry@assitia.com"
+  },
+  {
+    "id": "4",
+    "name": "Tate Wallace",
+    "email": "tatewallace@assitia.com"
+  },
+  {
+    "id": "5",
+    "name": "Kline Coffey",
+    "email": "klinecoffey@assitia.com"
+  },
+  {
+    "id": "6",
+    "name": "Marquez Middleton",
+    "email": "marquezmiddleton@assitia.com"
+  },
+  {
+    "id": "7",
+    "name": "Regina Rogers",
+    "email": "reginarogers@assitia.com"
+  },
+  {
+    "id": "8",
+    "name": "Maritza Shelton",
+    "email": "maritzashelton@assitia.com"
+  },
+  {
+    "id": "9",
+    "name": "Mcleod Rasmussen",
+    "email": "mcleodrasmussen@assitia.com"
+  }
+]
